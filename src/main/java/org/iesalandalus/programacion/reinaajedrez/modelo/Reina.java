75cca70761c875c80fe25330933918f16c17c0bc
david motos olmedo <david@debian-BULLSEYE-live-builder-AMD64>
2022-11-20 02:35:27 +0100

    Reina()

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -5,12 +5,17 @@ public class Reina {
 	private Posicion posicion;
 	private Color color;
 	
+	public Reina() {
+		setPosicion(new Posicion(1,'d'));
+		setColor(Color.BLANCO);
+	}
+	
 	public Posicion getPosicion() {
 		return posicion;
 	}
 	public void setPosicion(Posicion posicion) {
 		if (posicion == null)
-			throw new NullPointerException();
+			throw new NullPointerException("Error");
 		
 		this.posicion = posicion;
 	}
@@ -25,3 +30,4 @@ public class Reina {
 	}
 	
 }
+//Crea un constructor por defecto para esta clase que cree una reina blanca en la posición '1d'. Realiza un commit.
