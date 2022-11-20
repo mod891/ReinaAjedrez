6a0867da54a97c264c746777fc30f3199e0d1f93
david motos olmedo <david@debian-BULLSEYE-live-builder-AMD64>
2022-11-20 02:15:53 +0100

    getter & setter posicion

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -4,4 +4,23 @@ public class Posicion {
 
 	private int fila;
 	private char columna;
+	
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		if (fila < 1 || fila > 8) 
+			throw new IllegalArgumentException();
+		this.fila = fila;
+		
+	}
+	public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		if ((int) columna < 97 || (int) columna > 104 )
+			throw new IllegalArgumentException();
+
+		this.columna = columna;
+	}
 }
