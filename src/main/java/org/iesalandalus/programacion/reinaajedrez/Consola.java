7edcf71c82dfeefd22f5995326074a9347a36c5f
david motos olmedo <david@debian-BULLSEYE-live-builder-AMD64>
2022-11-20 19:16:27 +0100

    menu

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -3,7 +3,15 @@ package org.iesalandalus.programacion.reinaajedrez;
 public class Consola {
 
 	private Consola() {}
-
 	
+	
+	public void mostrarMenu() {
+		System.out.println("\nMENU\n----\n");
+		System.out.println("1: Crear reina por defecto");
+		System.out.println("2: Crear reina eligiendo el color\n");
+		System.out.println("3: Mover\n");
+		System.out.println("4: Salir\n");
+		
+	}
 	
 }
