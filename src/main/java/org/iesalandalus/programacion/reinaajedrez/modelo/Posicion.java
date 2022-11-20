fa34abadc08dad4b42d784a1e7f1d63103a0b63e
david motos olmedo <david@debian-BULLSEYE-live-builder-AMD64>
2022-11-20 02:20:18 +0100

    equals & hashcode

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import java.util.Objects;
+
 public class Posicion {
 
 	private int fila;
@@ -39,5 +41,22 @@ public class Posicion {
 			throw new IllegalArgumentException();
 
 		this.columna = columna;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
 	}
 }
