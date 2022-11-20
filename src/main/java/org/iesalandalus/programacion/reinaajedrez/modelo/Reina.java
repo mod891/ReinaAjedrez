785d2d7c3211ccba48129b633e8dd8fe5f9ac413
david motos olmedo <david@debian-BULLSEYE-live-builder-AMD64>
2022-11-20 02:33:35 +0100

    get & set

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -5,4 +5,23 @@ public class Reina {
 	private Posicion posicion;
 	private Color color;
 	
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	public void setPosicion(Posicion posicion) {
+		if (posicion == null)
+			throw new NullPointerException();
+		
+		this.posicion = posicion;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		if (color == null)
+			throw new NullPointerException();
+		
+		this.color = color;
+	}
+	
 }
