9a4f237a6dc39a970c7b96f2d6e1f3a65f8fb775
david motos olmedo <david@debian-BULLSEYE-live-builder-AMD64>
2022-11-22 15:11:24 +0100

    mainappfunc

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -6,6 +6,23 @@ public class MainApp {
 	
 	private static Reina reina;
 	
+	private static void ejecutarOpcion(int opcion) {
+		switch (opcion) {
+		case 1: 
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		}
+	}
+	
+	private static void crearReinaDefecto() {
+		reina = new Reina();
+	}
+	
 	public static void main(String[] args) {
 		
 	}
