e56a0d1b8ccce843d1e83eaac5679414dce73703
david motos olmedo <david@debian-BULLSEYE-live-builder-AMD64>
2022-11-22 15:29:47 +0100

    mover

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/MainApp.java
@@ -1,6 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
 import org.iesalandalus.programacion.reinaajedrez.modelo.Color;
+import org.iesalandalus.programacion.reinaajedrez.modelo.Direccion;
 import org.iesalandalus.programacion.reinaajedrez.modelo.Reina;
 import org.iesalandalus.programacion.reinaajedrez.Consola;
 public class MainApp {
@@ -27,6 +28,19 @@ public class MainApp {
 	private static void crearReinaColor() {	
 		Color color = Consola.elegirColor();
 		reina = new Reina(color);
+	}
+	
+	private static void mover() {
+		Consola.mostrarMenuDirecciones();
+		Direccion direccion = Consola.elegirDireccion();
+		int pasos = Consola.elegirPasos();
+		try {
+			
+			reina.mover(direccion, pasos);
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	public static void main(String[] args) {
 		
